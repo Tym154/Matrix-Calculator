d95a48d6b7ac9cdcc56b0940a933f820e2b00ddc
Tym154 <159148113+Tym154@users.noreply.github.com>
2024-04-15 22:34:24 +0200

    Update matrixCAL.cpp

--- matrixCAL.cpp
+++ matrixCAL.cpp
@@ -6,14 +6,14 @@ using namespace std;
 vector<vector<double>> matrix2;
 vector<vector<double>> matrix1;
 
-void writeOut(int row, int colum, vector<vector<double>>& matrix);
-void fill(int row, int colum, vector<vector<double>>& matrix);
+void writeOut(int row, int colum, vector<vector<double>>& matrix); //Function to write out the contents of a matrix
+void fill(int row, int colum, vector<vector<double>>& matrix); //Function to fill the matrix with data
 
 int main() {
     int row1, colum1, row2, colum2;
 
     cin >> row1 >> colum1;
-    matrix1.resize(row1, vector<double>(colum1));
+    matrix1.resize(row1, vector<double>(colum1)); //resizing the matrixes
 
     cin >> row2 >> colum2;
     matrix2.resize(row2, vector<double>(colum2));
@@ -25,16 +25,16 @@ int main() {
 
 void writeOut(int colum, int row, vector<vector<double>>& matrix) {
     for(int i = 0; i < row; i++) {
-        for(int j = 0; j < colum; j++) {
+        for(int j = 0; j < colum; j++) {  //iterating through the array and writing out every single element 
             cout << matrix[i][j] << " ";
         }
-        cout << "\n";
+        cout << "\n"; //writing a new line every new row
     }
 }
 
 void fill(int colum, int row, vector<vector<double>>& matrix) {
     for(int i = 0; i < row; i++) {
-        for(int j = 0; j < colum; j++) {
+        for(int j = 0; j < colum; j++) {  //iterating through the array and filling the matrix
             cin >> matrix[i][j];
         }
     }
