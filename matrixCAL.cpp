b5baea372f59f8e62a0bcdf1e28365e8132c345a
Tym154 <159148113+Tym154@users.noreply.github.com>
2024-04-17 22:53:13 +0200

    Update matrixCAL.cpp

--- matrixCAL.cpp
+++ matrixCAL.cpp
@@ -8,6 +8,9 @@ vector<vector<double>> matrix1;
 
 void writeOut(int row, int colum, vector<vector<double>>& matrix); //Function to write out the contents of a matrix
 void fill(int row, int colum, vector<vector<double>>& matrix); //Function to fill the matrix with data
+void addition(int row1, int colum1, int row2, int colum2, vector<vector<double>>& matrix1, vector<vector<double>>& matrix2);
+void subtraction(int row1, int colum1, int row2, int colum2, vector<vector<double>>& matrix1, vector<vector<double>>& matrix2);
+void multiplication(int row1, int colum1, int row2, int colum2, vector<vector<double>>& matrix1, vector<vector<double>>& matrix2);
 
 int main() {
     int row1, colum1, row2, colum2;
@@ -21,8 +24,8 @@ int main() {
 
     fill(row1, colum1, matrix1);
     fill(row2, colum2, matrix2);
-    addition(row1, colum1, row2, colum2, matrix1, matrix2);
-    writeOut(row1, colum1, ResultMatrix);
+    multiplication(row1, colum1, row2, colum2, matrix1, matrix2);
+    writeOut(row2, colum1, Bmatrix);
 }
 
 void writeOut(int row, int colum, vector<vector<double>>& matrix) {
@@ -74,3 +77,33 @@ void subtraction(int row1, int colum1, int row2, int colum2, vector<vector<doubl
     }
 }
 
+void multiplication(int row1, int colum1, int row2, int colum2, vector<vector<double>>& matrix1, vector<vector<double>>& matrix2){
+    if(colum1 == row2){
+        Amatrix.resize(row1, vector<double>(colum2));
+        for(int i = 0; i < row1; i++){
+            for(int j = 0; j < colum2; j++){
+                double temp = 0;
+                for(int k = 0; k < colum1; k++){
+                    temp += matrix1[i][k] * matrix2[k][j];
+                }
+                Amatrix[i][j] = temp;
+            }
+        }
+    }
+    if(colum2 == row1){
+        Bmatrix.resize(row2, vector<double>(colum1));
+        for(int i = 0; i < row2; i++){
+            for(int j = 0; j < colum1; j++){
+                double temp = 0;
+                for(int k = 0; k < colum2; k++){
+                    temp += matrix2[i][k] * matrix1[k][j];
+                }
+                Bmatrix[i][j] = temp;
+            }
+        }
+    }
+    else{
+        cout << "\nError\n"; //error if the matrixes sizes do not match
+        return;
+    }
+}
